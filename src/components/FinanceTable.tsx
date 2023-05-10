f156bac4700ec71610e7e281d1bbf20b38906d8d
Simon Metsi <metzzarn@gmail.com>
2023-05-10 17:08:27 +0200

    Set total in footer of tables

--- src/components/FinanceTable.tsx
+++ src/components/FinanceTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FinancialEntry } from 'redux/common';
 import {
   DataGrid,
@@ -8,12 +8,20 @@ import {
   GridPreProcessEditCellProps,
   GridRenderEditCellParams,
   GridRowModel,
+  GridSlotsComponentsProps,
 } from '@mui/x-data-grid';
 import { isValidNumber, maxLength, requiredMaxLength } from 'utils/validation';
-import { styled, Tooltip, tooltipClasses, TooltipProps } from '@mui/material';
-import { currencySymbol } from 'utils/numberUtils';
+import {
+  Box,
+  styled,
+  Tooltip,
+  tooltipClasses,
+  TooltipProps,
+  Typography,
+} from '@mui/material';
+import { currencySymbol, formatPrice } from 'utils/numberUtils';
 import { useAppSelector } from 'hooks';
-import { selectCurrency } from 'redux/settingsSlice';
+import { selectCurrency, selectDecimalPlaces } from 'redux/settingsSlice';
 
 interface AmountTableProps {
   data: FinancialEntry[];
@@ -26,8 +34,26 @@ interface AmountTableProps {
   removeRow: (index: number) => void;
 }
 
+declare module '@mui/x-data-grid' {
+  interface FooterPropsOverrides {
+    total: string;
+  }
+}
+
 export const FinanceTable = (props: AmountTableProps) => {
   const currency = useAppSelector(selectCurrency);
+  const decimalPlaces = useAppSelector(selectDecimalPlaces);
+
+  const [total, setTotal] = React.useState(
+    formatPrice('0,0', decimalPlaces, currency)
+  );
+
+  useEffect(() => {
+    const total = props.data.reduce((acc, curr) => {
+      return acc + Number(curr.value);
+    }, 0);
+    setTotal(formatPrice(total.toString(), decimalPlaces, currency));
+  });
 
   const StyledTooltip = styled(({ className, ...props }: TooltipProps) => (
     <Tooltip {...props} classes={{ popper: className }} />
@@ -113,12 +139,31 @@ export const FinanceTable = (props: AmountTableProps) => {
     };
   });
 
+  const CustomFooterStatusComponent = (
+    props: NonNullable<GridSlotsComponentsProps['footer']>
+  ) => {
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+        }}
+      >
+        <Typography sx={{ width: '100%', p: 1 }} align={'right'}>
+          Total {props.total}
+        </Typography>
+      </Box>
+    );
+  };
+
   return (
     <div style={{ width: '700px' }}>
       <DataGrid
         sx={{ mt: 1 }}
         columns={columns}
         rows={rows}
+        // hideFooter={true}
+        hideFooterPagination={true}
+        hideFooterSelectedRowCount={true}
         disableColumnSelector
         disableRowSelectionOnClick
         processRowUpdate={async (newRow: GridRowModel) => {
@@ -134,6 +179,12 @@ export const FinanceTable = (props: AmountTableProps) => {
         onCellClick={(params: GridCellParams) =>
           params.field === 'remove' && props.removeRow(Number(params.id))
         }
+        slots={{
+          footer: CustomFooterStatusComponent,
+        }}
+        slotProps={{
+          footer: { total },
+        }}
       />
     </div>
   );
