a6b879535130443ec8c1a52084774dfbbcd5eb3d
Simon Metsi <metzzarn@gmail.com>
2023-05-11 12:08:50 +0200

    Fix build directory

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import svgrPlugin from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  base: '/economy-planner-cra/',
   server: {
-    open: '/economy-planner-cra',
+    open: true,
   },
 });
